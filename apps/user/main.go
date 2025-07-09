3031c0739e018b190c799caed2049d2e85bed7d9
xigongyan <1992704557@qq.com>
2025-07-09 11:09:12 +0800

    feat(app/user)增加了数据库连接和etcd注册部分

--- apps/user/main.go
+++ apps/user/main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"fmt"
 	user "github.com/123508/xservergo/kitex_gen/user/userservice"
+	"github.com/123508/xservergo/pkg/config"
+	db "github.com/123508/xservergo/pkg/database"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 	"log"
 )
 
 func main() {
-	svr := user.NewServer(new(UserServiceImpl))
+	mysqlDB, err := db.InitMySQLDB()
 
-	err := svr.Run()
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	redisDB, err := db.InitRedisDB()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	if mysqlDB != nil && redisDB != nil {
+		fmt.Println("数据库初始化成功")
+	}
+
+	r, err := etcd.NewEtcdRegistryWithAuth(config.Conf.EtcdConfig.Endpoints, config.Conf.EtcdConfig.Username, config.Conf.EtcdConfig.Password)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svr := user.NewServer(new(UserServiceImpl),
+		server.WithRegistry(r),
+		server.WithServerBasicInfo(
+			&rpcinfo.EndpointBasicInfo{
+				ServiceName: config.Conf.UserConfig.ServiceName,
+			},
+		),
+	)
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
