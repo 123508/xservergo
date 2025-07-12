e47db60baa53fdd7b7e1109402ddb7050080bc24
123508 <973699883@qq.com>
2025-07-12 18:30:23 +0800

    feat(pkg/models) 创建AuditFields结构体并到用户和用户组模型

---
+++ pkg/models/audit.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+	"github.com/123508/xservergo/pkg/util"
+)
+
+type AuditFields struct {
+	CreatedAt time.Time `gorm:"column:created_at;comment '创建时间'"`
+	UpdatedAt time.Time `gorm:"column:updated_at;comment '更新时间'"`
+	DeletedAt *time.Time `gorm:"column:deleted_at;comment '删除时间(软删除)'"`
+	IsDeleted int8       `gorm:"column:is_deleted;comment '是否被删除'"`
+	Version   int        `gorm:"column:version;comment '版本号'"`
+	CreatedBy *util.UUID `gorm:"column:created_by;comment '创建人ID'"`
+	UpdatedBy *util.UUID `gorm:"column:updated_by;comment '修改人ID'"`
+}
