f10e8ca6c31a76a9c455c095a57dc004f556fe8a
123508 <973699883@qq.com>
2025-07-14 20:32:12 +0800

    fix(pkg/component): 更新allowedFields以包含所有必要字段

--- pkg/component/pub/defination.go
+++ pkg/component/pub/defination.go
@@ -20,12 +20,55 @@ type ItemType[E IntegerNumber] interface {
 
 // 只允许特定字段名
 var allowedFields = map[string]bool{
-	"id":         true,
-	"name":       true,
-	"created_at": true,
-	"updated_at": true,
-	"deleted_at": true,
-	"user_id":    true,
+	// 基础字段
+	"id":          true,
+	"name":        true,
+	"code":        true,
+	"description": true,
+	"status":      true,
+
+	// 用户相关字段
+	"user_id":  true,
+	"username": true,
+	"nickname": true,
+	"email":    true,
+	"phone":    true,
+	"gender":   true,
+	"avatar":   true,
+	"password": true,
+
+	// 权限和角色字段
+	"role_id":       true,
+	"group_id":      true,
+	"permission_id": true,
+	"parent_id":     true,
+	"type":          true,
+	"resource":      true,
+	"method":        true,
+	"path":          true,
+	"operator_id":   true,
+
+	// 登录相关字段
+	"login_type":   true,
+	"login_status": true,
+	"fail_reason":  true,
+	"login_ip":     true,
+	"user_agent":   true,
+	"device":       true,
+
+	// 时间戳字段
+	"created_at":       true,
+	"updated_at":       true,
+	"deleted_at":       true,
+	"is_deleted":       true,
+	"deleted_date":     true,
+	"deleted_at_fixed": true,
+
+	// 审计字段
+	"version":         true,
+	"created_by":      true,
+	"updated_by":      true,
+	"last_updated_by": true,
 }
 
 func IsValidField(field string) bool {
