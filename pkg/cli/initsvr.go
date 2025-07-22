1c693be0241b6973e0585fd56735b93cdbb11a5e
123508 <973699883@qq.com>
2025-07-22 22:29:35 +0800

    fix(pkg/cli): 更改user服务和auth服务初始化，使用etcd注册中心

--- pkg/cli/initsvr.go
+++ pkg/cli/initsvr.go
@@ -3,31 +3,55 @@ package cli
 import (
 	"github.com/123508/xservergo/kitex_gen/auth/authservice"
 	"github.com/123508/xservergo/kitex_gen/user/userservice"
+	"github.com/123508/xservergo/pkg/config"
 	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/discovery"
+	etcd "github.com/kitex-contrib/registry-etcd"
 	"time"
 )
 
-func InitUserService() userservice.Client {
-	c, err := userservice.NewClient("bitjump.douyinshop.user",
-		client.WithHostPorts("127.0.0.1:8902"),
-		client.WithRPCTimeout(3*time.Second))
+// initEtcdRegistry 初始化etcd注册中心
+func initEtcdRegistry() (discovery.Resolver, error) {
+	return etcd.NewEtcdResolverWithAuth(
+		config.Conf.EtcdConfig.Endpoints,
+		config.Conf.EtcdConfig.Username,
+		config.Conf.EtcdConfig.Password,
+	)
+}
 
+// InitUserService 初始化用户服务客户端
+func InitUserService() userservice.Client {
+	r, err := initEtcdRegistry()
 	if err != nil {
 		panic(err)
-		return nil
+	}
+
+	c, err := userservice.NewClient(
+		config.Conf.UserConfig.ServiceName,
+		client.WithRPCTimeout(3*time.Second),
+		client.WithResolver(r),
+	)
+	if err != nil {
+		panic(err)
 	}
 
 	return c
 }
 
+// InitAuthService 初始化认证服务客户端
 func InitAuthService() authservice.Client {
-	c, err := authservice.NewClient("bitjump.douyinshop.auth",
-		client.WithHostPorts("127.0.0.1:8903"),
-		client.WithRPCTimeout(3*time.Second))
-
+	r, err := initEtcdRegistry()
 	if err != nil {
 		panic(err)
-		return nil
+	}
+
+	c, err := authservice.NewClient(
+		config.Conf.AuthConfig.ServiceName,
+		client.WithRPCTimeout(3*time.Second),
+		client.WithResolver(r),
+	)
+	if err != nil {
+		panic(err)
 	}
 
 	return c
